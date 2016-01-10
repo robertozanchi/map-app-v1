95dbd0be6bd10f644859e4362fef792e20c4eace
robertozanchi <r.zanchi@gmail.com>
2016-01-10 15:00:12 +0100

    Show names of locations

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Model: hard coded location data
 var markers = [
 	{
-	"title": 'Totto Ramen',
+	"name": 'Totto Ramen',
 	"lat": '40.764777',
 	"lng": '-73.987641',
 	"description": 'Bustling Japanese noodle specialist with sit-down dining not far from its original storefront.'
 	},
 	{
-	"title": 'Lincoln Center for the Performing Arts',
+	"name": 'Lincoln Center for the Performing Arts',
 	"lat": '40.772874',
 	"lng": '-73.983479',
 	"description": 'Multi-venue complex home to many prominent groups like Metropolitan Opera & New York City Ballet.'
@@ -24,7 +24,7 @@ function LoadMap() {
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
- 
+
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 	//Create and open InfoWindow.
@@ -43,7 +43,7 @@ function LoadMap() {
 		(function (marker, data) {
 			google.maps.event.addListener(marker, "click", function (e) {
 			//Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
-			infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+			infoWindow.setContent("<b>" + data.name + "</b><br>" + "<div style = 'width:200px;min-height:60px'>" + data.description + "</div>");
 			infoWindow.open(map, marker);
 			});
 		})(marker, data);
@@ -55,15 +55,21 @@ function LoadMap() {
 //     //
 // };
 
+// Extracts location names from markers model
+var LocationName = function(data) {
+  this.name = ko.observable(data.name);
+};
+
 // ViewModel
 var ViewModel = function() {
 	var self = this;
+    
     // call the Place constructor here
     self.locationsArray = ko.observableArray([]);
     markers.forEach(function(locationItem){
-    self.locationsArray.push(locationItem.name);
+    	self.locationsArray.push(new LocationName(locationItem));
   });
+
 };
 
 ko.applyBindings(new ViewModel());
-
